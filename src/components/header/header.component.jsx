06ce438ec5921526f60bf9f8397e779e2eeeb6c4
Jun <sakakibarajun4@gmail.com>
2020-01-24 14:29:43 -0300

    google sign in button style

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { auth } from "../../firebase/firebase.utils";
+
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
 
-const Header = () => (
-  <div className='header'>
+const Header = ({ currentUser }) => (
+  <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
     </Link>
@@ -17,6 +19,15 @@ const Header = () => (
       <Link className="option" to="/contact">
         CONTACT
       </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
     </div>
   </div>
 );
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -33,7 +33,7 @@ class SignIn extends React.Component {
     return (
       <div className="sign-in">
         <h2>I have already a account</h2>
-        <span>Sign in with your email and passowrd</span>
+        <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
           <FormInput
@@ -52,9 +52,12 @@ class SignIn extends React.Component {
             label="password"
             required
           />
-
-          <CustomButton type="submit"> Sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle}> Sign in with google</CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit"> Sign in</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
