0adcd5343b41472e29fbc1299b07ce88fe07463d
Jun <sakakibarajun4@gmail.com>
2020-02-13 23:32:49 -0300

    redirect to checkout page on click of dropdown button and fixed some typos

--- src/redux/user/user.selectors.js
+++ src/redux/user/user.selectors.js
@@ -1,6 +1,6 @@
 import { createSelector } from "reselect";
 
-const selectUser = state => state.selectUser;
+const selectUser = state => state.user;
 
 export const selectCurrentUser = createSelector(
   [selectUser],
