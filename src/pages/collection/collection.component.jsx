2a02f4fce0152b21786f29159a81b3d41783bfc1
Jun <sakakibarajun4@gmail.com>
2020-02-22 23:59:57 -0300

    collection routing

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import { selectCollection } from "../../redux/shop/";
 
 import "./collection.styles.scss";
 
@@ -10,4 +13,8 @@ const CollectionPage = ({ match }) => (
   </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
